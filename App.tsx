cb403d34d4c9fc90e8c1b06dd70971d5ce5c74cb
castro <igordc38@gmail.com>
2025-11-11 23:04:51 -0300

    metadata import correction

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import "reflect-meta-data"
+import "reflect-metadata"
 import "@core/IoC/IoC.container"
 import React, {useEffect, useState} from "react";
 import * as SplashScreen from "expo-splash-screen";
